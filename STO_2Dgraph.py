41e281302ddbe24d5a52782bc5b7cced231c986b
pontacard <81417109+pontacard@users.noreply.github.com>
2023-12-07 19:53:11 +0900

    Add files via upload

--- STO_2Dgraph.py
+++ STO_2Dgraph.py
@@ -24,11 +24,18 @@ class one_spin_2D(STO_spin):
         #plt.savefig("reverse_|S|.png")
         #plt.plot(t,x)
         plt.plot(t, x, label="Sx")
-        #plt.savefig("reverse_x.png")
-        plt.show()
+        plt.xlabel("time(ns)")
+        plt.ylabel("Sx")
+        plt.savefig(f"2DSTO_Sx_{self.omega[1]}GHz_{self.STO_ac_effH[1]}Adiv^2.pdf")
+        plt.clf()
 
         plt.plot(t, z, label="Sz")
-        plt.show()
+        plt.xlabel("time(ns)")
+        plt.ylabel("Sz")
+        plt.savefig(f"2DSTO_Sz_{self.omega[1]}GHz_{self.STO_ac_effH[1]}Adiv^2.pdf")
+        #plt.show()
+
+        plt.clf()
 
         N = len(x)  # サンプル数
         f_s = 400  # サンプリングレート f_s[Hz] (任意)
@@ -38,23 +45,22 @@ class one_spin_2D(STO_spin):
         freq = np.fft.fftfreq(N, d=dt)  # 周波数を割り当てる（※後述）
         Amp = abs(y_fft / (N / 2))  # 音の大きさ（振幅の大きさ）
         plt.plot(freq[1:int(N / 2)], Amp[1:int(N / 2)])  # A-f グラフのプロット
-        # plt.xscale("log")  # 横軸を対数軸にセット
+        #plt.xscale("log")  # 横軸を対数軸にセット
+        plt.xlim(-3,80)
         plt.xlabel("GHz")
+        plt.savefig(f"2DSTO_Fourier_Sx_{self.omega[1]}GHz_{self.STO_ac_effH[1]}Adiv^2.pdf")
 
-        plt.show()
-        plt.plot(t, y, label = "Sy")
-        #plt.savefig("reverse_y.png")
-        #plt.show()
-        #plt.plot(t, z,  label = "Sz")
-        plt.ylim(-1.1,1.1)
+        plt.clf()
 
-        plt.xlabel("time(ns)")
-        plt.ylabel("S")
-        #plt.axhline(color = 'k')
-        plt.legend()
+        y_fft = np.fft.fft(z)  # 離散フーリエ変換
+        freq = np.fft.fftfreq(N, d=dt)  # 周波数を割り当てる（※後述）
+        Amp = abs(y_fft / (N / 2))  # 音の大きさ（振幅の大きさ）
+        plt.plot(freq[1:int(N / 2)], Amp[1:int(N / 2)])  # A-f グラフのプロット
+        plt.xlim(-3,80)
+        plt.xlabel("GHz")
+        plt.savefig(f"2DSTO_Fourier_Sz_{self.omega[1]}GHz_{self.STO_ac_effH[1]}Adiv^2.pdf")
+        plt.clf()
 
-        #plt.savefig("no_dump_xy.pdf")
-        plt.show()
 
 
 
@@ -70,7 +76,7 @@ if __name__ == '__main__':
     mu_0 = 1.26
     mu_h_div_2e = [0.824, -21]
     sta_M = [1.2, 0]  # 飽和磁化(T)で入れる
-    jac = [3.5, 12]
+    jac = [3.38, 12]
     d = [2, -9]
     Hacn = mu_h_div_2e[0] * jac[0] / (sta_M[0] * d[0])
     Haco = mu_h_div_2e[1] + jac[1] - (sta_M[1] + d[1])
